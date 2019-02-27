29a75cb6e60302a6bd9bac3cdb09c75479e73e3a
Spencer Walden <masquarainian@live.com>
2019-02-27 14:52:25 -0800

    Uses helper functions defined in bashrc

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-ylw='\e[33m'
-rst='\e[0m'
-dim='\e[2m'
+print_warn "This script will perform apt-get updating and so will require sudo password"
 
-echo -e "$ylw[*] This script will perform apt-get updating and so will require sudo password!$rst"
+set -ex
 sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
-echo -e "$rst"
