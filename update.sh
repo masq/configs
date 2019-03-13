a63a289a13d5b10578f5d1593bf58f1254935423
spencer walden <spencer@icebrg.io>
2019-03-12 18:11:14 -0700

    Changing lines on the update script so it's more clear at a glance

--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ set -e
 print_warn "This script will perform apt-get updating and so will require sudo password"
 
 set -ex
-sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get autoremove -y
